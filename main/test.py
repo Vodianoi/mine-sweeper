e5feea38b1ba5fa9b3ebc8f177704b6ab4a15614
axpoty <axel.poty@le-campus-numerique.fr>
2024-05-31 11:59:49 +0200

    count mine up and down

--- main/test.py
+++ main/test.py
@@ -16,7 +16,7 @@ class TestMinesweeper(unittest.TestCase):
         self.assertEqual(minesweeper(".*."), "1*1")
         self.assertEqual(minesweeper("*.*"), "*2*")
         self.assertEqual(minesweeper("*..*.*..*....*"), "*11*2*11*1001*")
-        # self.assertEqual(minesweeper("*..\n..*"), "*21\n12*")
+        self.assertEqual(minesweeper("*..\n..*"), "*21\n12*")
 
     # def final_test(self):
     #     self.assertEqual(minesweeper("*...\n....\n.*..\n...."), "*100\n2210\n1*10\n1110")
@@ -33,31 +33,45 @@ def minesweeper(field):
 def solve_lines(lines):
 
     solved_lines = []
-    for line in lines:
+    for y in range(len(lines)):
         solved_line = ""
+        line = lines[y]
         for x in range(len(line)):
-            if is_mine(line, x):
+            if is_mine(lines, x, y):
                 solved_line += "*"
             else:
-                nb_mine = count_mines_around(x, line)
-
+                nb_mine = count_mines_around(x, y, lines)
                 solved_line += str(nb_mine)
+
         solved_lines.append(solved_line)
     return solved_lines
 
 
-def count_mines_around(x, line):
+def count_mines_around(x, y, lines):
     nb_mine = 0
-    if is_mine(line, x + 1):
+    if is_mine(lines, x + 1, y + 1):
         nb_mine += 1
-    if is_mine(line, x - 1):
+    if is_mine(lines, x - 1, y + 1):
+        nb_mine += 1
+    if is_mine(lines, x + 1, y - 1):
+        nb_mine += 1
+    if is_mine(lines, x - 1, y - 1):
+        nb_mine += 1
+    if is_mine(lines, x - 1, y):
+        nb_mine += 1
+    if is_mine(lines, x + 1, y):
+        nb_mine += 1
+    if is_mine(lines, x, y + 1):
+        nb_mine += 1
+    if is_mine(lines, x, y - 1):
         nb_mine += 1
     return nb_mine
 
 
-def is_mine(line, x):
-    if x < 0:
+def is_mine(lines, x, y):
+    if y < 0 or y >= len(lines):
         return False
-    if x >= len(line):
+    line = lines[y]
+    if x < 0 or x >= len(line):
         return False
     return line[x] == "*"
