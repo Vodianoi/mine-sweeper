79d8bbef9c1b4a1b8b20d6b3b4f46196e08afe82
axpoty <axel.poty@le-campus-numerique.fr>
2024-05-31 12:08:33 +0200

    final

--- main/test.py
+++ main/test.py
@@ -18,57 +18,44 @@ class TestMinesweeper(unittest.TestCase):
         self.assertEqual(minesweeper("*..*.*..*....*"), "*11*2*11*1001*")
         self.assertEqual(minesweeper("*..\n..*"), "*21\n12*")
 
-    # def final_test(self):
-    #     self.assertEqual(minesweeper("*...\n....\n.*..\n...."), "*100\n2210\n1*10\n1110")
-    #     self.assertEqual(minesweeper("**...\n.....\n.*..."), "**100\n33200\n1*100")
+    def final_test(self):
+        self.assertEqual(minesweeper("*...\n....\n.*..\n...."), "*100\n2210\n1*10\n1110")
+        self.assertEqual(minesweeper("**...\n.....\n.*..."), "**100\n33200\n1*100")
 
 
 def minesweeper(field):
     lines = field.split('\n')
     solved_lines = solve_lines(lines)
-
     return "\n".join(solved_lines)
 
 
 def solve_lines(lines):
-
     solved_lines = []
     for y in range(len(lines)):
         solved_line = ""
         line = lines[y]
         for x in range(len(line)):
-            if is_mine(lines, x, y):
+            if is_mine(x, y, lines):
                 solved_line += "*"
             else:
                 nb_mine = count_mines_around(x, y, lines)
                 solved_line += str(nb_mine)
-
         solved_lines.append(solved_line)
     return solved_lines
 
 
 def count_mines_around(x, y, lines):
     nb_mine = 0
-    if is_mine(lines, x + 1, y + 1):
-        nb_mine += 1
-    if is_mine(lines, x - 1, y + 1):
-        nb_mine += 1
-    if is_mine(lines, x + 1, y - 1):
-        nb_mine += 1
-    if is_mine(lines, x - 1, y - 1):
-        nb_mine += 1
-    if is_mine(lines, x - 1, y):
-        nb_mine += 1
-    if is_mine(lines, x + 1, y):
-        nb_mine += 1
-    if is_mine(lines, x, y + 1):
-        nb_mine += 1
-    if is_mine(lines, x, y - 1):
-        nb_mine += 1
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            if is_mine(x + i, y + j, lines):
+                nb_mine += 1
     return nb_mine
 
 
-def is_mine(lines, x, y):
+def is_mine(x, y, lines):
     if y < 0 or y >= len(lines):
         return False
     line = lines[y]
